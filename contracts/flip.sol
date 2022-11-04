5a3c6f1f6175ec767f64f7aebe7e3121053c2f73
Ryan Gibb <ryangibb321@gmail.com>
2022-11-04 22:00:05 +0000

    consistent delegatecall storage layout

--- contracts/flip.sol
+++ contracts/flip.sol
@@ -2,7 +2,14 @@
 pragma solidity ^0.8.14;
 
 contract CoinFlip {
+  mapping (address => uint256) public consecutiveWins;
+  mapping (address => uint256) claimed;
+  uint256 lastHash;
   uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+  address immutable CamHack;
+  address immutable owner;
+  uint256 immutable public deployedAt;
 
   function flip() public returns (bool) {
     uint256 blockValue = uint256(blockhash(block.number - 1));
